76cccd183a752c71bfe10a48090343c98b704c1a
Милохин Семен <Semen.Milokhin@yandex.ru>
2019-08-02 18:11:24 +0300

    end of checkboxes and radio btns layout

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-	initQuizSelects(); 
+	initQuizSelects();
+	initQuizCheckboxes();
+	initCheckInput();
 
 	function initQuizSelects() {
 		var selects = $('.quiz__select');
@@ -48,6 +50,89 @@ $(document).ready(function(){
 					closeSelect();
 				}
 			});
+		});
+	}
+
+	function initQuizCheckboxes() {
+		var radioBtns = $('.quiz__radio-btns').find('.quiz__radio-input'),
+			checkboxes = $('.quiz__checkboxes').find('.quiz__checkbox-input'),
+			uncheck = function(array,number) {
+				array.each(function(iter,elem){
+					if (iter !== number) {
+						$(elem).parent('label').removeClass($(elem).parent('label')[0].classList[0]+'_checked');
+					}
+				});
+			},
+			iterate = function(array) {
+				if (array.length > 0) {
+
+					array.each(function(i,el){
+						if ($(el).prop('checked')) {
+							$(el).parent('label').addClass($(el).parent('label')[0].classList[0]+'_checked');
+						}
+					});
+
+					array.each(function(i,el){
+						$(el).on('change', function(){
+							var inputParent   = $(el).parent('label'),
+								parentClass   = inputParent[0].classList[0],
+								checkedClass  = parentClass+'_checked';
+
+							if (parentClass==='quiz__radio') {
+								uncheck(array,i);
+							}
+
+							if ($(el).prop('checked')) {
+								inputParent.addClass(checkedClass);
+							} else {
+								inputParent.removeClass(checkedClass);
+							}
+
+							if (parentClass==='quiz__radio') {
+								var checkInput = $('.quiz__check-input');
+								if (checkInput.hasClass('quiz__check-input_checked')) {
+									checkInput.removeClass('quiz__check-input_checked');
+									checkInput.find('.quiz__check-input-field').val('');
+								}
+							}
+						});
+					});
+				}
+			};
+			
+		iterate(radioBtns);
+		iterate(checkboxes);
+	}
+
+	function initCheckInput() {
+		var checkInput = $('.quiz__check-input'),
+			checkInputParent = checkInput.parent('div'),
+			btn = checkInput.find('.quiz__check-input-point'),
+			field = checkInput.find('.quiz__check-input-field');
+
+
+		btn.on('click', function(){
+			if (field.val()==='') {
+				field.focus();
+			} else {
+				field.val('');
+				checkInput.removeClass('quiz__check-input_checked');
+			}
+		})
+
+		field.on('keyup', function(evt){
+			if (field.val()==='') {
+				checkInput.removeClass('quiz__check-input_checked');
+			} else if (field.val()!==''&&!checkInput.hasClass('quiz__check-input_checked')&&!checkInputParent.hasClass('quiz__radio-btns')) {
+				checkInput.addClass('quiz__check-input_checked');
+			} else if (field.val()!==''&&!checkInput.hasClass('quiz__check-input_checked')&&checkInputParent.hasClass('quiz__radio-btns')) {
+				checkInputParent.find('.quiz__radio-input').each(function(i,el){
+					if ($(el).prop('checked')) {
+						$(el).prop('checked',false).change();
+					}
+				});
+				checkInput.addClass('quiz__check-input_checked');
+			}
 		});
 	}
 });
